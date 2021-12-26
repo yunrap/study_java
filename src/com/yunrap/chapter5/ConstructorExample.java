6fce968808afff8de33d21893d257789c2225096
yunrap <dbsdpwl7@gmail.com>
2021-12-26 11:46:31 +0900

    생성자 생

--- src/com/yunrap/chapter5/ConstructorExample.java
+++ src/com/yunrap/chapter5/ConstructorExample.java
@@ -3,6 +3,11 @@ package com.yunrap.chapter5;
 public class ConstructorExample {
 	public ConstructorExample()
 	{
-		System.out.println("helloss");
+		System.out.println("생성자는 클래스이름이랑 같게한다");
+	}
+	
+	public ConstructorExample(String str)
+	{
+		System.out.println("두번째 생성자는 파라미터를더해주었다 " +str);
 	}
 }
