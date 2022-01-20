d0299a7d759bf8389238fda27a142f59fdcd068a
yunrap <dbsdpwl7@gmail.com>
2022-01-20 23:56:57 +0900

    indexof 학습

---
+++ Test/study_java/src/com/yunrap/practice/IndexOf.java
@@ -0,0 +1,14 @@
+package study_java.src.com.yunrap.practice;
+
+//indexOf : 입력값이 문자열에 없다면, 리턴 -1
+//메서드에 문자열을 입력하면 위치에따라 숫자로 반환한다.
+
+public class IndexOf {
+    public static void main(String[] args){
+        String str = "Hello world";
+        System.out.println(str.indexOf("H"));       //위치에따라 숫자로 반환한다.
+        System.out.println(str.indexOf("o"));       // 4
+        System.out.println(str.indexOf('a'));      //값이없을땐, -1
+
+    }
+}
