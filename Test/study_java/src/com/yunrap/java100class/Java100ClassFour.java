22dce36c295c18c6243d62a3b4732acd78037227
yunrap <dbsdpwl7@gmail.com>
2022-02-04 07:09:51 +0900

    다형성 객체생성방법

--- Test/study_java/src/com/yunrap/java100class/Java100ClassFour.java
+++ Test/study_java/src/com/yunrap/java100class/Java100ClassFour.java
@@ -247,3 +247,33 @@ public class Java100ClassFour {
 //그런데, 그 반대는 안된다.
 //왜냐하면, "앵무새"는 분명 그 상위인 "새"라고 말할 수 있지만, 새는 종류가 많기때문에 모든 새가 앵무새는 아니기때문이다.
 //따라서, 상위(부모)클래스의 인스턴스(객체)는 하위(자식) 클래스의 인스턴스(객체)로 사용될수없다.
+
+
+
+
+//### ch4 : 다형성 객체 생성방법
+// 다음중 객체 생성 방법이 틀린 것을 골라보시오.
+// 앞서 다형성(polymorphism)의 개념에 대해서 살펴봤는데 코드를 통해서 실습해보는 문제이다.
+
+class Person {}
+class Student extends Person{}
+
+
+public class Java100ClassFour {
+    public static void main(String[] args){
+        //[1] : 객체 생성
+        Student s1 = new Student(); //지극히 정상 --> 에러날이유가 하나도없다.
+
+        //[2] : 객체 생성 --> 타입은 부모 타입으로 생성
+        Person s2 = new Student();  //이것도 정상 --> 하위(자식)클래스로 객체를 만들면서 타입은 상위(부모) 타입을 쓰는 것이 가능 --> 다형성
+
+        //[3] : 객체 생성
+        Person aaa = new Person();   //지극히 정상
+
+        //[4] : 객체 생성 --> 상위(부모) 클래스로 객체를 생성하면서 타입은 하위(자식) 타입을 쓰는 경우 --> Err
+        //Student bbb = new Person(); //Err
+    }
+}
+
+
+
